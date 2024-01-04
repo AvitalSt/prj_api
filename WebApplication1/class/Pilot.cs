a3cf51d967f8e33a6333a103db461de5b716078f
AvitalSt <avitalsty@gmail.com>
2024-01-04 18:24:16 +0200

    add summary

--- WebApplication1/class/Pilot.cs
+++ WebApplication1/class/Pilot.cs
@@ -1,6 +1,8 @@
 ﻿namespace WebApplication1
 {    public class Pilot
-    {
+    {/// <summary>
+    /// njhjhbmn
+    /// </summary>
         public int Id { get; set; }
         public string Name { get; set; }
         public int NumWorker { get; set; }
