304d3539377e5d0bbce612c509a6911adf475918
AvitalSt <avitalsty@gmail.com>
2024-01-04 18:22:15 +0200

    commit all

---
+++ WebApplication1/class/Flight.cs
@@ -0,0 +1,12 @@
+﻿namespace WebApplication1
+{
+    public class Flight
+    {
+        public string FlightNum { get; set; }
+        public DateTime Date { get; set; }
+        public DateTime LeavingTime { get; set; }
+        public DateTime ArrivalTime { get; set; }
+        public int TerminalNum { get; set; }
+
+    }
+}
---
+++ WebApplication1/class/Passenger.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApplication1
+{
+    public class Passenger
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string CountryOrigin { get; set; }
+        public string DestinationCountry { get; set; }
+        public int NumBag { get; set; }
+    }
+}
