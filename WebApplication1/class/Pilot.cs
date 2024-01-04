dac8aeb1d48e961d5ff228e1d37a7975d65d1898
AvitalSt <avitalsty@gmail.com>
2024-01-04 18:25:08 +0200

    by

--- WebApplication1/class/Pilot.cs
+++ WebApplication1/class/Pilot.cs
@@ -1,8 +1,6 @@
 ﻿namespace WebApplication1
 {    public class Pilot
-    {/// <summary>
-    /// njhjhbmn
-    /// </summary>
+    {
         public int Id { get; set; }
         public string Name { get; set; }
         public int NumWorker { get; set; }
